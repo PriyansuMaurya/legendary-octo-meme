bd5b155272e05555330d004b389a2c9052fd7583
PriyanshuMaurya <mauryapriyanshu-cmpn@atharvacoe.ac.in>
2022-12-07 04:24:52 -0800

    Replaced url and name

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://aliabdaal.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Priyanshu
+          I m Ali
         </a>
       </header>
     </div>
