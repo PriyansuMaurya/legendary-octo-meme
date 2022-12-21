604ecdd25083de26e803addf6be35118558adc28
PriyanshuMaurya <mauryapriyanshu-cmpn@atharvacoe.ac.in>
2022-12-22 00:37:24 +0530

    created page to showoff blender works and added input box for taking name and email along  with btn and also added resume

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { Route, BrowserRouter as Routes, useRoutes } from "react-router-dom";
+import reportWebVitals from "./reportWebVitals";
+import BlenderWorks from "./components/BlenderWork";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const RoutedApp = () => {
+  const routes = useRoutes([
+    { path: "/", element: <App /> },
+    { path: "/blender", element: <BlenderWorks /> },
+  ]);
+
+  return routes;
+};
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Routes>
+    <RoutedApp/>
+  </Routes>
 );
 
 // If you want to start measuring performance in your app, pass a function
